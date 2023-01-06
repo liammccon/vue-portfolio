9f77410dbcfe6bb06a3c285402b864b0b71e3faa
liammccon <liam.t.mccon@gmail.com>
2023-01-06 18:04:39 -0500

    Clean up NavBar spacing and add comments

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,43 @@
 <template>
+    <!--Navbar-->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container-fluid">
-      <!---->
-      <a class="navbar-brand fs-2" href="#">Liam</a>
-      <!---->
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-sm-0 fs-2 ">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Media</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Calendar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Coding</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Other</a>
-          </li>
-          
-        </ul>
-      </div>
-    </div>
-  </nav>
-  </template>
+        <div class="container-fluid">
+            <!--Left Name Text-->
+            <a class="navbar-brand fs-2" href="#">Liam</a>
+            
+            <!--Toggle button-->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <!--Collapsing Links-->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-sm-0 fs-2 ">
+                    
+                    <!--Links-->
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Coding</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Music</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Media</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Calendar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Fun</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
   
   <script>
   export default {
