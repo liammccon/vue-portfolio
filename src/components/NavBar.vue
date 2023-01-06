e6e9dba26a1ad95a9fc18473c42535b77e19db59
liammccon <liam.t.mccon@gmail.com>
2023-01-06 16:11:19 -0500

    Add previously made navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <!---->
+      <a class="navbar-brand fs-2" href="#">Liam M.</a>
+      <!---->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0 fs-2 ">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Media</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Calendar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Coding</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Other</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar',
+  }
+  </script>
+  
+  <style scoped>
+    .navbar{
+      font-family: 'Yeseva One', cursive;
+  
+    }
+  </style>
