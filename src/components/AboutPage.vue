676431b4f007a7a6a14878bbf7d60269e659e37d
liammccon <liam.t.mccon@gmail.com>
2023-01-08 21:16:46 -0500

    Change text

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -4,10 +4,11 @@
     <div class="container-fluid ps-xs-1 pe-xs-1 ps-md-4 pt-3 pt-lg-5">
         <div class="row mt-lg-4">
             <div class="col-lg-8 text-start pt-lg-3">
-                <h1 class="text-center pb-1 border-dark border-bottom">About me</h1> 
-                <h3 class="mt-3">I am a musician and programmer, finishing my undergrad with a major in music and a minor in computer science.</h3>
+                <h1 class="text-center display-1 pb-1 border-dark border-bottom">About me</h1> 
+                <!--TODO-->
+                <p class="h3 mt-3 pb-1 text-center border-dark border-bottom">I am a musician and programmer, finishing my undergrad with a major in music and a minor in computer science.</p>
                 
-                <div class="fs-5 mt-2">
+                <div class="fs-5 mt-3">
                     <p>I am in my last semester at the Cleveland Insitute of Music and Case Western Reserve University.</p>
                     <p>I'm a freelance french hornist in the Cleveland area and have played with orchestras in the greater Cleveland area such as Canton, Akron, Mansfield, and Firelands.
                     I have also played with the grammy-winning Cleveland Chamber Symphony. 
@@ -16,6 +17,7 @@
                     I have experience in Java, JavaScript, HTML, CSS, Python, and more. In fall 2022, I worked as a Teacher's Assistant at CWRU to teach 
                     Java and software principals to undergraduate students. 
                     </p>
+                    
                 </div>
                 
             </div>
